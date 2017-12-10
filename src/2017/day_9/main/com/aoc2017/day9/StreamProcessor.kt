7e2473464287eb88cc1ec3ac5cc1e2e76cd804ed
Rick Gale <richardgale90@gmail.com>
2017-12-10 17:27:18 +0000

    Finished day 9

--- src/2017/day_9/main/com/aoc2017/day9/StreamProcessor.kt
+++ src/2017/day_9/main/com/aoc2017/day9/StreamProcessor.kt
@@ -1,5 +1,7 @@
 package com.aoc2017.day9
 
+import java.util.*
+
 object StreamProcessor {
 
     fun FilterCancelledCharacters(input: String): String {
@@ -7,13 +9,47 @@ object StreamProcessor {
         return input.replace(cancelRegex, "")
     }
 
-    fun FilterGarbage(input: String): String {
-        val garbageRegex = Regex("<[^>]*>")
-        return FilterCancelledCharacters(input).replace(garbageRegex, "")
+    fun FilterGarbage(input: String): Pair<String, Int> {
+        val garbageRegex = Regex("<([^>]*)>")
+        val postCancelled = FilterCancelledCharacters(input)
+        val postFiltered = postCancelled.replace(garbageRegex, "")
+        val garbageChars = garbageRegex.findAll(postCancelled).map { it.groupValues[1].length }.sum()
+
+        return Pair(postFiltered, garbageChars)
     }
 
-    fun ParseStream(stream: String): Group {
-        return Group()
+    fun ParseStream(stream: String): Pair<Group, Int> {
+        val filterResult = FilterGarbage(stream.trim())
+        val groupStack = Stack<Group>()
+        var lastPoppedGroup: Group? = null
+
+        filterResult.first.forEach {
+            when (it) {
+                '{' -> {
+                    if (groupStack.size > 0) {
+                        val last = groupStack.last()
+                        val group = Group(level = last.level + 1, parent = last)
+                        last.children.add(group)
+                        groupStack.push(group)
+                    } else {
+                        groupStack.push(Group())
+                    }
+                }
+                '}' -> {
+                    if (groupStack.size > 0) {
+                        lastPoppedGroup = groupStack.pop()
+                    } else {
+                        throw RuntimeException("Unexpected close bracket")
+                    }
+                }
+                ',' -> {}
+                else -> throw RuntimeException("Unexpected character: $it")
+            }
+        }
+
+        return Pair(
+                lastPoppedGroup?: throw RuntimeException("No group found in stream"),
+                filterResult.second)
     }
 
 }
