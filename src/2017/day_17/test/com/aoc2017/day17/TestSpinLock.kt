ab4abeeed164136af2e5539bc033ed5b734aa850
Rick Gale <richardgale90@gmail.com>
2017-12-17 14:25:59 +0000

    Finished day 17

--- src/2017/day_17/test/com/aoc2017/day17/TestSpinLock.kt
+++ src/2017/day_17/test/com/aoc2017/day17/TestSpinLock.kt
@@ -1,12 +1,79 @@
 package com.aoc2017.day17
 
+import org.testng.Assert.assertEquals
 import org.testng.annotations.Test
 
 class TestSpinLock {
 
     @Test
-    fun testSpinLock() {
-        
+    fun testInsert() {
+        val sl = SpinLock(3)
+
+        sl.insertNext()
+        assertEquals(sl.values, listOf(0, 1))
+        assertEquals(sl.currPos, 1)
+
+        sl.insertNext()
+        assertEquals(sl.values, listOf(0, 2, 1))
+        assertEquals(sl.currPos, 1)
+
+        sl.insertNext()
+        assertEquals(sl.values, listOf(0, 2, 3, 1))
+        assertEquals(sl.currPos, 2)
+
+        sl.insertNext()
+        assertEquals(sl.values, listOf(0, 2, 4, 3, 1))
+        assertEquals(sl.currPos, 2)
+
+        sl.insertNext()
+        assertEquals(sl.values, listOf(0, 5, 2, 4, 3, 1))
+        assertEquals(sl.currPos, 1)
+
+        sl.insertNext()
+        assertEquals(sl.values, listOf(0, 5, 2, 4, 3, 6, 1))
+        assertEquals(sl.currPos, 5)
+    }
+
+    @Test
+    fun testValueAfterLast() {
+        val sl = SpinLock(3)
+        repeat(2017) {
+            sl.insertNext()
+        }
+
+        assertEquals(sl.values[sl.currPos + 1], 638)
+    }
+
+    @Test
+    fun testActualValueAfterLast() {
+        val sl = SpinLock(316)
+        repeat(2017) {
+            sl.insertNext()
+        }
+
+        println(sl.values[sl.currPos + 1])
+    }
+
+    @Test
+    fun testActualLargeValueAfterLast() {
+        var currSize = 1
+        var currPos = 0
+        var answer = 0
+        val steps = 316
+
+        repeat(50_000_000) {
+            val insValue = it + 1
+            val nextPos = ((currPos + steps) % currSize) + 1
+
+            if (nextPos == 1) {
+                answer = insValue
+            }
+
+            currPos = nextPos
+            currSize++
+        }
+
+        println(answer)
     }
 
 }
