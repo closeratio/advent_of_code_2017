09b72b1b83cbbc5bdc8c3aeb99053d5b9d6d048e
Rick Gale <Rick Gale@localhost>
2017-12-17 14:25:59 +0000

    Finished day 17

--- src/2017/day_17/main/com/aoc2017/day17/SpinLock.kt
+++ src/2017/day_17/main/com/aoc2017/day17/SpinLock.kt
@@ -1,4 +1,21 @@
 package com.aoc2017.day17
 
-class SpinLock {
+class SpinLock(
+        val stepSize: Int
+) {
+
+    val values = ArrayList<Int>(50_000_000).apply {
+        add(0)
+    }
+
+    var currPos = 0
+
+    fun insertNext() {
+        val next = values.max()!! + 1
+
+        val ind = ((currPos + stepSize) % values.size) + 1
+        currPos = ind
+        values.add(ind, next)
+    }
+
 }
